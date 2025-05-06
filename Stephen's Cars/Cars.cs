b98dbbc0e09e3842e945558f775d1c346a221b04
ssheridan20 <stephen.sheridan29@student.qcc.cuny.edu>
2025-05-06 19:29:18 -0400

    Add project files.

---
+++ Stephen's Cars/Cars.cs
@@ -0,0 +1,24 @@
+﻿namespace Stephen_s_Cars
+{
+    internal class Cars
+    {
+        //started out with automatic properties
+        public string CarType {  get; set; }
+        public string CarName { get; set; }
+        public double MPG {  get; set; }
+
+        //default constructor - no parameters
+        public Cars() 
+        { 
+            CarType = string.Empty;
+            CarName = string.Empty;
+            MPG = 0;
+        }
+        public Cars(string type, string name, double mpg)
+        {
+            CarType = type;
+            CarName = name;
+            MPG = mpg;
+        }
+    }
+}
